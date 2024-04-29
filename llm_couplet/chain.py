3bafbdb2fda0b07d9af05ff3b952586c1e3c6ecb
habaneraa <Lumberjack1024@yeah.net>
2024-04-29 22:41:12 +0800

    refactoring

--- llm_couplet/chain.py
+++ llm_couplet/chain.py
@@ -8,14 +8,14 @@ from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from openai import APIError
 
-from llm_couplet.prompt_templates import (analyze_pos, analyze_senti,
-                                          analyze_tones, analyze_topic,
-                                          chars_prompt,
-                                          compose_couplet_messages,
-                                          compose_couplet_single)
+from llm_couplet import prompt_templates
 from llm_couplet.utils import count_chinese_characters
 
 LLMConfig = namedtuple("LLMConfig", ["model", "api_key", "base_url", "temperature"])
+
+
+def get_num_characters(s):
+    return prompt_templates.chars_prompt.format(count_chinese_characters(s))
 
 
 class CoupletComposer:
@@ -26,27 +26,24 @@ class CoupletComposer:
         self.single = self.build_single()
 
     def build_chain(self):
-        # first_llm = OpenAI(**self.first_llm_config._asdict())
         first_llm = ChatOpenAI(**self.first_llm_config._asdict())
         last_llm = ChatOpenAI(**self.last_llm_config._asdict())
         output_parser = StrOutputParser()
-        get_num_characters = lambda s: chars_prompt.format(count_chinese_characters(s))
+
+        prompt_pos = ChatPromptTemplate.from_messages(prompt_templates.analyze_pos)
+        prompt_tones = ChatPromptTemplate.from_messages(prompt_templates.analyze_tones)
+        prompt_topic = ChatPromptTemplate.from_messages(prompt_templates.analyze_topic)
+        prompt_senti = ChatPromptTemplate.from_messages(prompt_templates.analyze_senti)
         return (
             {
                 "chars": itemgetter("first_line") | RunnableLambda(get_num_characters),
-                "pos": ChatPromptTemplate.from_messages(analyze_pos) | first_llm | output_parser,
-                "tones": ChatPromptTemplate.from_messages(analyze_tones)
-                | first_llm
-                | output_parser,
-                "topic": ChatPromptTemplate.from_messages(analyze_topic)
-                | first_llm
-                | output_parser,
-                "senti": ChatPromptTemplate.from_messages(analyze_senti)
-                | first_llm
-                | output_parser,
+                "pos": prompt_pos | first_llm | output_parser,
+                "tones": prompt_tones | first_llm | output_parser,
+                "topic": prompt_topic | first_llm | output_parser,
+                "senti": prompt_senti | first_llm | output_parser,
                 "first_line": itemgetter("first_line"),
             }
-            | ChatPromptTemplate.from_messages(compose_couplet_messages)
+            | ChatPromptTemplate.from_messages(prompt_templates.compose_couplet_messages)
             | last_llm
             | output_parser
         )
@@ -54,7 +51,8 @@ class CoupletComposer:
     def build_single(self):
         last_llm = ChatOpenAI(**self.last_llm_config._asdict())
         output_parser = StrOutputParser()
-        return ChatPromptTemplate.from_template(compose_couplet_single) | last_llm | output_parser
+        prompt = ChatPromptTemplate.from_template(prompt_templates.compose_couplet_single)
+        return prompt | last_llm | output_parser
 
     def compose(self, first_line: str) -> str:
         return self.chain.invoke({"first_line": first_line})
